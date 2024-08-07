b35c01c0c1f662528e53a354e7c39cb8d253e9d5
Nalan Kurnaz <nalan.kurnaz99@gmail.com>
2024-08-07 13:14:14 -0400

    encryption&decryption

---
+++ app.py
@@ -0,0 +1,28 @@
+###encryption
+def encrypt(text, key):
+    text = text.upper()
+    key = key.upper()
+    encrypted_text = []
+
+    numbered_key = [ord(i) - ord('A') for i in key]
+    numbered_text = [ord(i) - ord('A') for i in text]
+
+    for i in range(len(numbered_text)):
+        numbered_text[i] = chr(((numbered_text[i] + numbered_key[i % len(numbered_key)]) % 26) + ord('A'))
+        encrypted_text.append(numbered_text[i])
+    return ''. join(encrypted_text)
+        
+
+###decryption
+def decrypt(text, key):
+    text = text.upper()
+    key = key.upper()
+    decrypted_text = []
+
+    numbered_key = [ord(i) - ord('A') for i in key]
+    numbered_text = [ord(i) - ord('A') for i in text]
+
+    for i in range(len(numbered_text)):
+        numbered_text[i] = chr(((numbered_text[i] - numbered_key[i % len(numbered_key)]) % 26) + ord('A'))
+        decrypted_text.append(numbered_text[i])
+    return ''. join(decrypted_text)
